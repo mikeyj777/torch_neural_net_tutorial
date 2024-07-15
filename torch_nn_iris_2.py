f8f3a5c79650473b238370b98005746d150664ce
Michael James <mikeyj777@gmail.com>
2024-07-15 17:13:04 -0400

    update

--- torch_nn_iris_2.py
+++ torch_nn_iris_2.py
@@ -4,6 +4,12 @@ import torch.nn.functional as F
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# parameters
+rand_seed = 41
+lr = 0.01
+epochs = 100
 
 # create model class that inherits nn.Module
 class Model(nn.Module):
@@ -33,9 +39,57 @@ class Model(nn.Module):
         return x
 
 # set your seed
-torch.manual_seed(41)
+torch.manual_seed(rand_seed)
 
 model = Model()
 
 url = 'https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv'
 my_df = pd.read_csv(url)
+
+# change last column.  str -> float
+dict_map = {
+    'Setosa': 0,
+    'Versicolor': 1,
+    'Virginica': 2,
+}
+
+my_df['variety'] = my_df['variety'].map(dict_map)
+
+# Train Test Split!
+
+# drop the outcome col.  isolate input features.
+# get numpy vals
+X = my_df.drop('variety', axis=1)
+X = X.values
+
+y = my_df['variety'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rand_seed)
+
+X_train = torch.FloatTensor(X_train)
+X_test = torch.FloatTensor(X_test)
+y_train = torch.LongTensor(y_train) # long tensor = 64-bit int
+y_test = torch.LongTensor(y_test)
+
+# set criterion to measure error, how far off predictions are from 
+criterion = nn.CrossEntropyLoss()
+
+# choose Adam optimizer, set learning rate
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+losses = []
+
+for _ in range(epochs):
+    # get predicted results
+    y_pred = model.forward(X,X_train)
+
+    # measure loss
+    loss = criterion(y_pred, y_train)
+    losses.append(loss)
+
+    
+
+
+
+apple = 1
